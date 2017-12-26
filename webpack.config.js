bbe2bb88b9e17c96f078bb6d5b1110254bd30811
Adam Chen Wei <adamchenwei@gmail.com>
2017-12-25 21:47:36 -0500

    chore(): change dist

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   entry: SOURCE_DIR + '/index.js',
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: 'index_bundle.js'
+    filename: 'index.js'
   },
   module: {
     loaders: [
