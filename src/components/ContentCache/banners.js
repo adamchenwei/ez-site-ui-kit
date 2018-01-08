699b6e7bedfc8c2c8b67ef41e178ec60b05856e5
Adam Chen Wei <adamchenwei@gmail.com>
2018-01-08 09:01:07 -0500

    fix(route): ref

--- src/components/ContentCache/banners.js
+++ src/components/ContentCache/banners.js
@@ -2,25 +2,25 @@ module.exports = [
   {
     id: 0,
     content: {
-      url: './assets/images/banner/Banner 1.jpg',
+      url: '/assets/images/banner/Banner 1.jpg',
     }
   },
   {
     id: 1,
     content: {
-      url: './assets/images/banner/Banner 2.jpg',
+      url: '/assets/images/banner/Banner 2.jpg',
     }
   },
   {
     id: 2,
     content: {
-      url: './assets/images/banner/Banner 3.jpg',
+      url: '/assets/images/banner/Banner 3.jpg',
     }
   },
   {
     id: 3,
     content: {
-      url: './assets/images/banner/Banner 4.jpg',
+      url: '/assets/images/banner/Banner 4.jpg',
     }
   },
 ];
