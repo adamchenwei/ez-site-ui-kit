7feb5f5de398cb6442658077fde469d3191c2ad3
Adam Chen Wei <adamchenwei@gmail.com>
2018-02-07 22:07:36 -0500

    feat(): model related

--- src/components/CatalogItem/index.js
+++ src/components/CatalogItem/index.js
@@ -18,7 +18,7 @@ export default class CatalogItem extends Component {
     } = this.props;
     return (
       <Card>
-        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+        <CardImg top width="100%" src="/assets/images/logo.png" alt="Card image cap" />
         <CardBody>
           <CardTitle>{title}</CardTitle>
           <CardSubtitle>{route.webRoute}</CardSubtitle>
