bd097867b7070fa2950c41ecbe3259623917efaa
Adam Chen Wei <adamchenwei@gmail.com>
2018-02-07 22:08:08 -0500

    chore(): remove

--- c.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "Commit all $1 with $2"
-git status
-git add *
-git commit -m "$1(): $2 "
-git push origin master
-
-#git add * && git commit -m "chore(): seo" &&  git push origin master
